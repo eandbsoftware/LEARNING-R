d58df4bfc76fae38abe8ec62f78b48246e82ce0c
Enrico Besenyei <enrico.besenyei@voluntis.com>
2022-12-18 11:49:23 +0100

    update vectors

--- basics/vectors.R
+++ basics/vectors.R
@@ -27,3 +27,27 @@ print(v)
 # If the final element specified does not belong to the sequence then it is discarded.
 v <- 3.8:11.4
 print(v)
+
+# Create vector with elements from 5 to 9 incrementing by 0.4.
+print(seq(5, 9, by = 0.4))
+
+# The logical and numeric values are converted to characters.
+s <- c('apple','red',5,TRUE)
+print(s)
+
+# Accessing vector elements using position.
+t <- c("Sun","Mon","Tue","Wed","Thurs","Fri","Sat")
+u <- t[c(2,3,6)]
+print(u)
+
+# Accessing vector elements using logical indexing.
+v <- t[c(TRUE,FALSE,FALSE,FALSE,FALSE,TRUE,FALSE)]
+print(v)
+
+# Accessing vector elements using negative indexing.
+x <- t[c(-2,-5)]
+print(x)
+
+# Accessing vector elements using 0/1 indexing.
+y <- t[c(0,0,0,0,0,0,1)]
+print(y)
