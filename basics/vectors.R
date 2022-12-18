1324b46282cbf17ed7b8da260dd0db30bef59a09
Enrico Besenyei <enrico.besenyei@voluntis.com>
2022-12-18 11:28:22 +0100

    add basics for R

---
+++ basics/vectors.R
@@ -0,0 +1,29 @@
+# Atomic vector of type character.
+print("abc");
+
+# Atomic vector of type double.
+print(12.5)
+
+# Atomic vector of type integer.
+print(63L)
+
+# Atomic vector of type logical.
+print(TRUE)
+
+# Atomic vector of type complex.
+print(2+3i)
+
+# Atomic vector of type raw.
+print(charToRaw('hello'))
+
+# Creating a sequence from 5 to 13.
+v <- 5:13
+print(v)
+
+# Creating a sequence from 6.6 to 12.6.
+v <- 6.6:12.6
+print(v)
+
+# If the final element specified does not belong to the sequence then it is discarded.
+v <- 3.8:11.4
+print(v)
