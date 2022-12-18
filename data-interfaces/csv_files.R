c7918263efd037efe298df3e053925ba233adf80
Enrico Besenyei <enrico.besenyei@voluntis.com>
2022-12-18 12:42:05 +0100

    update R for CSV files

--- data-interfaces/csv_files.R
+++ data-interfaces/csv_files.R
@@ -38,3 +38,25 @@ sal <- max(data$salary)
 # Get the person detail having max salary.
 retval <- subset(data, salary == max(salary))
 print(retval)
+
+# Create a data frame.
+data <- read.csv("c:/workspace/LEARNING-R/data-interfaces/input/sample1.csv")
+print(data)
+
+#Get the persons in IT department whose salary is greater than 600
+info <- subset(data, salary > 600 & dept == "IT")
+print(info)
+
+#Get the people who joined on or after 2014
+retval <- subset(data, as.Date(start_date) > as.Date("2014-01-01"))
+print(retval)
+
+#Writing to CSV file
+# Create a data frame.
+data <- read.csv("c:/workspace/LEARNING-R/data-interfaces/input/sample1.csv")
+retval <- subset(data, as.Date(start_date) > as.Date("2014-01-01"))
+
+# Write filtered data into a new file.
+write.csv(retval,"c:/workspace/LEARNING-R/data-interfaces/output/output.csv")
+newdata <- read.csv("c:/workspace/LEARNING-R/data-interfaces/output/output.csv")
+print(newdata)
